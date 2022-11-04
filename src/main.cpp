ccc12ee8c4afe3a1b75663a49db2bb8058b41ec6
Alwin Arrasyid <alwin.ridd@gmail.com>
2022-11-04 15:06:32 +0700

    added more generic include library utility function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 void setup()
 {
     Serial.begin(115200);
     while (!Serial);
+
+    Wire.begin();
 
     pinMode(24, OUTPUT);
 }
@@ -15,4 +18,17 @@ void loop()
     delay(100);
     digitalWrite(24, LOW);
     delay(100);
+
+   
+    for(int i = 1; i < 127; i++)
+    {
+        Wire.beginTransmission(i);
+        if (Wire.endTransmission(i) == 0)
+        {
+            Serial.print("Found device at address: ");
+            Serial.println(i, HEX);
+        }
+    }
+
+     delay(1000);
 }
